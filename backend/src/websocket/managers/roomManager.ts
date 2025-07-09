4d8def6b78f07bf44b27eb982f210c76c8cc13e4
Matheus Bispo Machtura <math.machtura@gmail.com>
2025-07-09 00:55:08 -0300

    feat: add manager websocket

---
+++ backend/src/websocket/managers/roomManager.ts
@@ -0,0 +1,55 @@
+import { Room } from "./Room";
+import { v4 as uuidv4 } from "uuid";
+
+class RoomManager {
+    private rooms: Map<string, Room>;
+
+    constructor() {
+        this.rooms = new Map();
+    }
+
+    createRoom(creatorName: string): Room {
+        const roomId = uuidv4().substring(0, 8);
+        const roomName = `Sala de ${creatorName}`;
+        const newRoom = new Room(roomId, roomName);
+        this.rooms.set(roomId, newRoom);
+        console.log(`Sala ${roomName} (${roomId}) criada.`);
+        return newRoom;
+    }
+
+    findRoomById(roomId: string): Room | undefined {
+        return this.rooms.get(roomId);
+    }
+
+    removeRoom(roomId: string) {
+        this.rooms.delete(roomId);
+        console.log(`Sala ${roomId} removida.`);
+    }
+
+    getPublicRooms() {
+        const publicRoomsData = [];
+        for (const room of this.rooms.values()) {
+            if (room.isPublic && room.state === "waiting") {
+                publicRoomsData.push({
+                    id: room.id,
+                    name: room.name,
+                    current_users: room.players.size,
+                    max_users: room.maxPlayers,
+                });
+            }
+        }
+        return publicRoomsData;
+    }
+
+    broadcastToRoom(roomId: string, message: string) {
+        const room = this.findRoomById(roomId);
+        if (room) {
+            for (const player of room.getPlayers()) {
+                player.ws.send(message);
+            }
+        }
+    }
+}
+
+// Exportamos uma única instância (padrão Singleton)
+export const roomManager = new RoomManager();
